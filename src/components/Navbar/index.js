494de2ccc6bad20f2f4e33549488bb71ff752030
lucas dalan <75147965+lucasd09@users.noreply.github.com>
2021-12-28 08:44:18 -0300

    installed packages

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,16 @@
-import { Container, Stack } from '@mui/material'
-import React from 'react'
+import { Button, Container, Stack } from '@mui/material'
+import React from 'react'   
 
 export default function Navbar() {
     return (
-        <Container sx={{display: 'flex', justifyContent: 'space-between'}}>
+        <Container sx={{display: 'flex', justifyContent: 'space-between', height: "100px"}}>
             <Stack direction='row' spacing={6} sx={{alignItems: 'center'}}>
                 <p>navbar</p>
                 <p>navbar</p>
                 <p>navbar</p>
             </Stack>
             <Stack direction='row' spacing={6} sx={{alignItems: 'center'}}>
-                <p>navbar</p>
-                <p>navbar</p>
-                <p>navbar</p>
+                <Button variant='outlined'>link with metamask</Button>
             </Stack>
         </Container>
     )
